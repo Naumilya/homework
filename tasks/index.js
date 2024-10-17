a765d35590ecd700b35806deaf3d77d501685cfd
Наумов Илья <ianaumov@awg.ru>
2024-10-17 21:59:11 +0300

    tasks: solved the problem and added a new one

--- tasks/index.js
+++ tasks/index.js
@@ -40,80 +40,92 @@
 
 // ? 5
 
-const orders = [
-	{
-		id: '24003874339',
-		status: 'Передано в SAP',
-		statusChangeTime:
-			'Sun Oct 24 2021 10:24:00 GMT+0300 (Москва, стандартное время)',
-	},
-	{
-		id: '010001247991',
-		status: 'Доставлен',
-		statusChangeTime:
-			'Mon May 24 2021 11:59:00 GMT+0300 (Москва, стандартное время)',
-	},
-	{
-		id: '13030778733',
-		status: 'Ожидает оплаты',
-		statusChangeTime:
-			'Mon Oct 25 2021 15:45:00 GMT+0300 (Москва, стандартное время)',
-	},
-	{
-		id: '010001247987',
-		status: 'Аннулирован',
-		statusChangeTime:
-			'Thu Dec 12 2019 19:52:00 GMT+0300 (Москва, стандартное время)',
-	},
-	{
-		id: 'DB000006008',
-		status: 'Новый',
-		statusChangeTime:
-			'Mon Oct 25 2021 16:50:00 GMT+0300 (Москва, стандартное время)',
-	},
-]
+// const orders = [
+// 	{
+// 		id: '24003874339',
+// 		status: 'Передано в SAP',
+// 		statusChangeTime:
+// 			'Sun Oct 24 2021 10:24:00 GMT+0300 (Москва, стандартное время)',
+// 	},
+// 	{
+// 		id: '010001247991',
+// 		status: 'Доставлен',
+// 		statusChangeTime:
+// 			'Mon May 24 2021 11:59:00 GMT+0300 (Москва, стандартное время)',
+// 	},
+// 	{
+// 		id: '13030778733',
+// 		status: 'Ожидает оплаты',
+// 		statusChangeTime:
+// 			'Mon Oct 25 2021 15:45:00 GMT+0300 (Москва, стандартное время)',
+// 	},
+// 	{
+// 		id: '010001247987',
+// 		status: 'Аннулирован',
+// 		statusChangeTime:
+// 			'Thu Dec 12 2019 19:52:00 GMT+0300 (Москва, стандартное время)',
+// 	},
+// 	{
+// 		id: 'DB000006008',
+// 		status: 'Новый',
+// 		statusChangeTime:
+// 			'Mon Oct 25 2021 16:50:00 GMT+0300 (Москва, стандартное время)',
+// 	},
+// ]
 
-const statuses = ['Новый', 'Ожидает оплаты', 'Передано в SAP', 'Доставлен']
-const nonChangebleStatuses = ['Доставлен', 'Аннулирован']
+// const statuses = ['Новый', 'Ожидает оплаты', 'Передано в SAP', 'Доставлен']
+// const nonChangebleStatuses = ['Доставлен', 'Аннулирован']
 
-function changeStatus(orderId) {
-	let order = orders.find(order => order.id === orderId)
+// function changeStatus(orderId) {
+// 	let order = orders.find(order => order.id === orderId)
 
-	if (!order) {
-		return {
-			status: 'error',
-			massage: `Заказ с "${orderId}" не найден`,
-		}
-	}
+// 	if (!order) {
+// 		return {
+// 			status: 'error',
+// 			massage: `Заказ с "${orderId}" не найден`,
+// 		}
+// 	}
 
-	if (nonChangebleStatuses.includes(order.status)) {
-		return {
-			status: 'error',
-			massage: `Ошибка статус заказа "${orderId}" не может быть изменен, потому что "${order.status}"`,
-		}
-	}
+// 	if (nonChangebleStatuses.includes(order.status)) {
+// 		return {
+// 			status: 'error',
+// 			massage: `Ошибка статус заказа "${orderId}" не может быть изменен, потому что "${order.status}"`,
+// 		}
+// 	}
 
-	const currentStatusIndex = statuses.indexOf(order.status)
+// 	const currentStatusIndex = statuses.indexOf(order.status)
 
-	if (currentStatusIndex === -1 || currentStatusIndex === statuses.length - 1) {
-		return {
-			status: 'error',
-			message: `Статус заказа "${orderId}" не может быть изменен из-за отсутствия следующего статуса.`,
-			statusChangeTime: order.statusChangeTime,
-		}
-	}
+// 	if (currentStatusIndex === -1 || currentStatusIndex === statuses.length - 1) {
+// 		return {
+// 			status: 'error',
+// 			message: `Статус заказа "${orderId}" не может быть изменен из-за отсутствия следующего статуса.`,
+// 			statusChangeTime: order.statusChangeTime,
+// 		}
+// 	}
 
-	const newStatus = statuses[currentStatusIndex + 1]
+// 	const newStatus = statuses[currentStatusIndex + 1]
 
-	order.status = newStatus
-	order.statusChangeTime = new Date().toString()
+// 	order.status = newStatus
+// 	order.statusChangeTime = new Date().toString()
 
-	return {
-		status: 'success',
-		massage: `Статус заказа "${orderId}" изменен на "${newStatus}"`,
-	}
-}
+// 	return {
+// 		status: 'success',
+// 		massage: `Статус заказа "${orderId}" изменен на "${newStatus}"`,
+// 	}
+// }
 
-console.log(changeStatus('DB000006008'))
-console.log(changeStatus('010001247987'))
-console.log(changeStatus('123456789012'))
+// console.log(changeStatus('DB000006008'))
+// console.log(changeStatus('010001247987'))
+// console.log(changeStatus('123456789012'))
+
+// ? 6
+
+// function plural(n) {
+//   return n != 1;
+// }
+
+// console.log(plural(0.5))
+// console.log(plural(1))
+// console.log(plural(100))
+
+// ? 7
